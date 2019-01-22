c34a8fc8e9548f46558a9a068897004edf8e9237
Arnaud Bailly <arnaud.oqube@gmail.com>
2019-01-22 22:44:19 +0100

    parse des atoms

--- src/LittleTyper.hs
+++ src/LittleTyper.hs
@@ -1,14 +1,36 @@
 module LittleTyper
     ( isAnAtom
+    , parse
+    , Thing(..)
     ) where
 
 import           Data.Char
+import           Text.Parsec hiding (parse)
+
+
+data Thing = Atom { atom :: String }
+           | SomethingElse
+  deriving (Eq, Show)
+
+-- partial
+parse :: String -> Either String Thing
+parse  input  =
+    case runParser littleTyperParser () "" input of
+        Right x -> Right x
+        Left e  -> Left $ show e
 
 isAnAtom :: String -> Bool
-isAnAtom ('\'':x:xs) = all isValidAtomCharacter (x:xs)
-  where
-    isValidAtomCharacter c
-        | isAlpha c = True
-        | c == '-'  = True
-        | otherwise = False
-isAnAtom _           = False
+isAnAtom s = case parse s of
+    Right (Atom _) -> True
+    Left _         -> False
+
+littleTyperParser :: Parsec String () Thing
+littleTyperParser = atomParser <* eof
+
+atomParser :: Parsec String () Thing
+atomParser = do
+    char '\''
+    s <- many1 validAtomCharacter
+    pure $ Atom s
+    where
+    validAtomCharacter = letter <|> char '-'
--- test/LittleTyperSpec.hs
+++ test/LittleTyperSpec.hs
@@ -6,7 +6,7 @@ import           Test.QuickCheck
 
 spec :: Spec
 spec = describe "The Little Typer" $
-  describe "The law of tick marks: Atoms" $
+  describe "Atoms" $ do
   it "an atom is a tick mark followed by one or more letters or hyphens" $ do
     isAnAtom "'atom" `shouldBe` True
     isAnAtom "'ratatouille" `shouldBe` True
@@ -16,3 +16,12 @@ spec = describe "The Little Typer" $
     isAnAtom "'coeur-d-artichauts" `shouldBe` True
     isAnAtom "'coeur_d_artichauts" `shouldBe` False
     isAnAtom "'αβγδ" `shouldBe` True
+
+  it "can be judged as an Atom" $ do
+    parse "'atom"`shouldBe` (Right $ Atom "atom")
+
+  it "compares 2 atoms" $ do
+    parse "'ratatouille" `shouldBe` parse "'ratatouille"
+
+  -- it "two atoms with the same characters are the 'same' Atom" $ do
+  --   return value
